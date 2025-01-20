9349ac0fd32f372c3025c9e61142ba9aa3b8ced9
Bartlomiej Kurzeja <bartlomiej+git@kurzeja.it>
2025-01-20 14:44:31 +0100

    Allow adding new matches

--- sportradar/scoreboard.py
+++ sportradar/scoreboard.py
@@ -1,5 +1,13 @@
 class ScoreBoard:
+    def __init__(self):
+        """Create in-memory storage"""
+        self._matches = []
+
     @property
-    def summary(self):
-        """Return summary of ongoing matches."""
-        return []
+    def summary(self) -> list[None]:
+        """Return summary of ongoing matches"""
+        return self._matches
+
+    def add(self, home_team: str, away_team: str):
+        """Add new match to score board"""
+        self._matches.append(None)
--- tests/test_scoreboard.py
+++ tests/test_scoreboard.py
@@ -4,3 +4,11 @@ def test_create_empty_scoreboard():
     """Test if empty score board has no matches"""
     board = ScoreBoard()
     assert board.summary == []
+
+def test_add_new_match_to_scoreboard():
+    """Test if new match is added"""
+    board = ScoreBoard()
+
+    board.add("Mexico", "Brazil")
+
+    assert len(board.summary) == 1
